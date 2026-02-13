77c482e37486e10e89531d2ca58922efc164a8ad
Mustafa <kh3reef@gmail.com>
2026-02-13 03:37:21 +0200

    update official_news.py

--- scripts/real_madrid/official_news.py
+++ scripts/real_madrid/official_news.py
@@ -64,8 +64,7 @@ def getArticleData(url):
                 pTags = descriptionContainer.find_all("p")
                 if not pTags:
                     continue
-                for p in pTags:
-                    desc += p.get_text(strip=True)
+		desc += pTags[0].get_text(strip=True)
 
         if desc:
             desc = "\n" + f"{desc[:800]}..." if len(desc) > 800 else desc + "\n"
