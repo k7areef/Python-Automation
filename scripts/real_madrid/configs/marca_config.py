b550854b1409d7baaf50d23af5c0eee4887f095d
Mustafa <kh3reef@gmail.com>
2026-09-18 03:01:39 +0300

    Edit Marca Config File

--- scripts/real_madrid/configs/marca_config.py
+++ scripts/real_madrid/configs/marca_config.py
@@ -1,7 +1,11 @@
-BASE_URL = "https://www.marca.com/"
+BASE_URL = "https://www.marca.com"
 NEWS_URL = f"{BASE_URL}/futbol/real-madrid.html"
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+        }
+        
 
 COLLECTION_NAME = "real_madrid_articles"
 SOURCE_NAME = "marca"
